db2a3ba53bc124f0ec4a0b66cc1fc857791fca26
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-01-12 09:36:39 -0300

    create pages (1)

---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">Profile Edit</div>
+    );
+  }
+}
+
+export default ProfileEdit;
