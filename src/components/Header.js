3f8e64a647e3186657dcc6f274fab385ebfc8796
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-01-13 09:57:39 -0300

    Navigation Links (4)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import trybeTunes from '../img/trybe-tunes.png';
 
 class Header extends React.Component {
   constructor() {
@@ -24,9 +26,17 @@ class Header extends React.Component {
     const { profileName, isLoading } = this.state;
     return (
       <header data-testid="header-component">
-        { isLoading
-          ? <Loading />
-          : <h2 data-testid="header-user-name">{ profileName }</h2>}
+        <div className="row">
+          <img src={ trybeTunes } alt="trybe tunes" />
+          { isLoading
+            ? <Loading />
+            : <h2 data-testid="header-user-name">{ profileName }</h2>}
+        </div>
+        <nav>
+          <Link data-testid="link-to-search" to="/search"> Pesquisa </Link>
+          <Link data-testid="link-to-favorites" to="/favorites"> Favoritas </Link>
+          <Link data-testid="link-to-profile" to="/profile"> Perfil </Link>
+        </nav>
       </header>
     );
   }
